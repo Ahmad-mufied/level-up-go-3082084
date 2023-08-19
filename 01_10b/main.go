845bc6131d2c6d3b4c9b9174e6bcdb4cae10d7eb
Ahmad-mufied <teranggelap01@gmail.com>
2023-08-20 02:20:47 +0700

    Calculator edge cases

--- 01_10b/main.go
+++ 01_10b/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"strconv"
 	"strings"
 )
+
+// Use pointers and errors to handle edge cases
+
+// The TASK
+// Given a simple calculator implement edge case and input verification that ouputs
+// error messages and gracefully shuts down the program.
 
 // operators is the map of legal operators and their functions
 var operators = map[string]func(x, y float64) float64{
@@ -16,25 +23,45 @@ var operators = map[string]func(x, y float64) float64{
 }
 
 // parseOperand parses a string to a float64
-func parseOperand(op string) float64 {
-	parsedOp, _ := strconv.ParseFloat(op, 64)
-	return parsedOp
+func parseOperand(op string) (*float64, error) {
+	parsedOp, err := strconv.ParseFloat(op, 64)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot parse: %v", err)
+	}
+	return &parsedOp, nil
 }
 
 // calculate returns the result of a 2 operand mathematical expression
-func calculate(expr string) float64 {
+func calculate(expr string) (*float64, error) {
 	ops := strings.Fields(expr)
-	left := parseOperand(ops[0])
-	right := parseOperand(ops[2])
-	f := operators[ops[1]]
-	result := f(left, right)
-	return result
+	nops := len(ops)
+	if nops != 3 {
+		return nil, fmt.Errorf("cannot calculate: need 3 operands, got %d", nops)
+	}
+	left, err := parseOperand(ops[0])
+	if err != nil {
+		return nil, err
+	}
+	right, err := parseOperand(ops[2])
+	if err != nil {
+		return nil, err
+
+	}
+	f, ok := operators[ops[1]]
+	if !ok {
+		return nil, fmt.Errorf("cannot calculate: %s is unknown", ops[1])
+	}
+	result := f(*left, *right)
+	return &result, nil
 }
 
 func main() {
 	expr := flag.String("expr", "",
 		"The expression to calculate on, separated by spaces.")
 	flag.Parse()
-	result := calculate(*expr)
-	log.Printf("%s = %.2f\n", *expr, result)
+	result, err := calculate(*expr)
+	if err != nil {
+		log.Fatalf("Error: %v\n", err)
+	}
+	log.Printf("%s = %.2f\n", *expr, *result)
 }
